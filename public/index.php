cd82dad27cd5e071d67447062b357e233f3c38d6
andikurniawan04 <andikurniawan.9a@gmail.com>
2025-07-15 18:57:11 +0700

    feat: change upload file size

--- public/index.php
+++ public/index.php
@@ -1,17 +1,22 @@
 <?php
 
+ini_set('max_execution_time', 0);
+ini_set('memory_limit', '4096M');
+ini_set('post_max_size', '100M');
+ini_set('upload_max_filesize', '100M');
+
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
+(require_once __DIR__ . '/../bootstrap/app.php')
     ->handleRequest(Request::capture());
